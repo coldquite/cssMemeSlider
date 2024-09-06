8128f5028fbb2e5bf4dce7464f2991593813eb89
Andrii <andriipigal@gmail.com>
2024-09-06 10:44:53 +0200

    feat: add pics db and slider's skeleton (Fri, Sep 6, 2024 10:46:43 AM)

---
+++ cssMemeSlider/meme_db.js
@@ -0,0 +1,22 @@
+db = [
+    {
+        'name' : 'blinking',
+        'text': 'When you realize the bug was just a typo',
+        'img' : './media/blinking.jpg',
+    },
+    {
+        'name' : 'doge',
+        'text': 'When someone asks if you tested your code and you say \'of course\'',
+        'img' : './media/doge.jpg',
+    },
+    {
+        'name' : 'fire',
+        'text': 'That moment when you realize Plan B is working perfectly',
+        'img' : './media/fire.jpg',
+    },
+    {
+        'name' : 'ok',
+        'text': 'When the universe decides to play a prank on you',
+        'img' : './media/ok.jpg',
+    },
+]
